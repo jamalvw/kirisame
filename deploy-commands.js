89c6b7850446342ba4338ff1e7a09b7509e51ec7
jamalvw <jvwill00@gmail.com>
2023-02-25 13:56:51 -0500

    Update command deployer

--- deploy-commands.js
+++ deploy-commands.js
@@ -15,8 +15,27 @@ for (const file of commandFiles)
 	commands.push(command.data.toJSON());
 }
 
+// Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(config.discord.token);
 
-rest.put(Routes.applicationCommands(config.discord.clientId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+// and deploy your commands!
+(async () =>
+{
+	try
+	{
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(config.discord.clientId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error)
+	{
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
